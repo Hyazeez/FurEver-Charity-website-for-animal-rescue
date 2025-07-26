df1f0862a66195d0c2904343471e3ff955d8a46a
Azeez shaban <hyazeez123@gmail.com>
2025-07-27 02:07:18 +0530

    Successfully completed

--- process.php
+++ process.php
@@ -7,14 +7,14 @@ include 'db.php';
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST['name'];
     $email = $_POST['email'];
-    $donations = $_POST['donations'];
+    $donation = $_POST['donation'];
     $message = $_POST['message'];
 
-    $sql = "INSERT INTO donations (name, email, amount, message) VALUES (?, ?, ?, ?)";
+    $sql = "INSERT INTO donations (name, email, donation, message) VALUES (?, ?, ?, ?)";
 
     $stmt = $conn->prepare($sql);
     if ($stmt) {
-        $stmt->bind_param("ssds", $name, $email, $donations, $message);
+        $stmt->bind_param("ssis", $name, $email, $donation, $message);
         if ($stmt->execute()) {
             echo "Application submitted successfully!";
         } else {
